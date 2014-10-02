c8c29109b4f44bf1b251c67b208c3cf7a6269692
Adam McMaster <adam@zooniverse.org>
2014-10-02 15:00:30 +0100

    Improve error handling/detection.
    * Don't follow HTTP redirects (if the container returned a redirect then
      it's running.
    * Print tracebacks to stdout for all errors to assist debugging failed
      containers.
    * Make the HTTP GET path configurable.

--- docker_status.py
+++ docker_status.py
@@ -7,6 +7,7 @@ import os
 import re
 import urllib2
 import sys
+import traceback
 
 OK_STATUSES = [ 200, 301, 302 ]
 HTTP_TIMEOUT = os.environ.get('HTTP_TIMEOUT', 30)
@@ -18,6 +19,14 @@ LISTEN_PORT = os.environ.get('LISTEN_PORT', '80')
 app = Flask(__name__)
 
 checks = {}
+
+class NoRedirectHTTPErrorProcessor(urllib2.HTTPErrorProcessor):
+    def http_response(self, request, response):
+        return response
+
+    https_response = http_response
+
+url_opener = urllib2.build_opener(NoRedirectHTTPErrorProcessor)
 
 @app.route("/")
 def status():
@@ -32,14 +41,16 @@ def status():
     return message, status
 
 def checker(host, status):
+    get_path = os.environ.get("%s_GET_PATH" % host, "/")
+
     while True:
         try:
-            result = urllib2.urlopen("http://%s/" % host, timeout=HTTP_TIMEOUT)
+            result = url_opener.open("http://%s%s" % (host, get_path),
+                                     timeout=HTTP_TIMEOUT)
             status.value = result.getcode()
-        except (urllib2.URLError) as e:
+        except Exception as e:
             status.value = getattr(e, 'code', -1)
-        except:
-            status.value = -1
+            traceback.print_exc()
 
         print datetime.datetime.now(), host, status.value
         sys.stdout.flush()
